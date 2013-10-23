52ac5248a67a1dc0156f512bd43f4f1ae6bc85a3
Brad Feehan <git@bradfeehan.com>
2013-10-23 15:49:29 +1100

    Add test coverage to Derelict#debug!

--- spec/derelict_spec.rb
+++ spec/derelict_spec.rb
@@ -16,4 +16,35 @@ describe Derelict do
       " INFO derelict: Creating and validating new instance for '/Applications/Vagrant'\n"
     ]}
   end
+
+  describe "#debug!" do
+    let(:enabled) { double("enabled") }
+    let(:level) { double("level") }
+    let(:logger) { Derelict.logger }
+    let(:stderr) { Log4r::Outputter.stderr }
+
+    subject { Derelict.debug! :enabled => enabled, :level => level }
+
+    context "when enabling debug mode" do
+      before do
+        expect(logger).to receive(:level=).with(level).and_return(nil)
+        expect(logger).to receive(:outputters).and_return(Array.new)
+        expect(logger).to receive(:add).with(stderr).and_return(logger)
+      end
+
+      let(:enabled) { true }
+      it { should be Derelict }
+    end
+
+    context "when disabling debug mode" do
+      before do
+        expect(logger).to receive(:level=).with(Log4r::OFF).and_return(nil)
+        expect(logger).to receive(:remove).with("stderr").and_return(logger)
+      end
+
+      let(:real_level) { Log4r::OFF }
+      let(:enabled) { false }
+      it { should be Derelict }
+    end
+  end
 end
