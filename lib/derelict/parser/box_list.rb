fdeefb35d133254dad3b8e6517d7bc856834165b
Brad Feehan <git@bradfeehan.com>
2013-11-27 13:29:54 +1100

    Add Derelict::Parser::BoxList

--- lib/derelict/parser/box_list.rb
+++ lib/derelict/parser/box_list.rb
@@ -2,5 +2,51 @@ module Derelict
   # Parses the output of "vagrant box list"
   class Parser::BoxList < Parser
     autoload :InvalidFormat, "derelict/parser/box_list/invalid_format"
+
+    # Include "memoize" class method to memoize methods
+    extend Memoist
+
+    # Output from "vagrant box list" if there are no boxes installed
+    NO_BOXES = <<-END.gsub(/^ {6}/, '')
+      There are no installed boxes! Use `vagrant box add` to add some.
+    END
+
+    # Regexp to parse a box line into a box name and provider
+    #
+    # Capture groups:
+    #
+    #   1. Box name, as listed in the output
+    #   2. Name of the provider for that box
+    PARSE_BOX = %r[
+      ^(.*)    # Box name starts at the start of the line
+      \ \(     # Provider is separated by a space and open-parenthesis
+        (.*)   # Provider name
+      \)$      # Ends with close-parenthesis and end-of-line
+    ]x         # Ignore whitespace to allow these comments
+
+    # Retrieves a Set containing all the boxes from the output
+    def boxes
+      box_lines.map {|l| parse_line l.match(PARSE_BOX) }.to_set
+    end
+
+    # Provides a description of this Parser
+    #
+    # Mainly used for log messages.
+    def description
+      "Derelict::Parser::BoxList instance"
+    end
+
+    private
+      # Retrieves an array of the box lines in the output
+      def box_lines
+        return [] if output.match NO_BOXES
+        output.lines
+      end
+
+      # Parses a single line of the output into a Box object
+      def parse_line(match)
+        raise InvalidFormat.new "Couldn't parse box list" if match.nil?
+        Derelict::Box.new *match.captures[0..1]
+      end
   end
 end
---
+++ spec/derelict/parser/box_list_spec.rb
@@ -0,0 +1,55 @@
+require "spec_helper"
+
+describe Derelict::Parser::BoxList do
+  subject { Derelict::Parser::BoxList.new output }
+  let(:output) { nil }
+
+  it "is autoloaded" do
+    should be_a Derelict::Parser::BoxList
+  end
+
+  describe "#boxes" do
+    subject { Derelict::Parser::BoxList.new(output).boxes }
+
+    include_context "logged messages"
+    let(:expected_logs) {[
+      "DEBUG boxlist: Successfully initialized Derelict::Parser::BoxList instance\n",
+    ]}
+
+    context "with valid output" do
+      let(:output) {
+        <<-END.gsub /^ +/, ""
+          foo (provider_one)
+          bar (provider_two)
+        END
+      }
+
+      let(:foo) { Derelict::Box.new "foo", "provider_one" }
+      let(:bar) { Derelict::Box.new "bar", "provider_two" }
+      it { should eq Set[foo, bar] }
+    end
+
+    context "with invalid output" do
+      let(:output) {
+        <<-END.gsub /^ +/, ""
+          foo (provider_one) lolwut
+          bar with no brackets
+        END
+      }
+
+      it "should raise InvalidFormat" do
+        expect { subject }.to raise_error Derelict::Parser::BoxList::InvalidFormat
+      end
+    end
+
+    context "with no boxes in list" do
+      let(:output) {
+        <<-END.gsub /^ +/, ""
+          There are no installed boxes! Use `vagrant box add` to add some.
+        END
+      }
+
+      it { should eq Set.new }
+    end
+  end
+end
